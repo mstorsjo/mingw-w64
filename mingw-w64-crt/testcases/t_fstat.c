e6a8bb1a5f10ba32525c648e8470b39b334d1c11
Pali Rohár <pali.rohar@gmail.com>
2025-12-18 00:15:00 +0100

    crt: Fix fstat() to signal overflow and return correct type for directories
    msvcrt _fstat function is broken and for directory fd it returns S_ISREG
    type. 32-bit pre-msvcr110 _fstat does not properly signal size overflow.
    UCRT seems to work correctly.
    
    Add mingw-w64 fstat wrappers around msvcrt _fstat functions which change
    S_IFMT to S_IFDIR if winapi filehandle has FILE_ATTRIBUTE_DIRECTORY
    attribute set. Provide wrapper for all 4 fstat size_t/time_t variants.
    For 32-bit pre-msvcr110 builds, adds similar workaround like for existing
    mingw-w64 stat functions.

--- mingw-w64-crt/testcases/t_fstat.c
+++ mingw-w64-crt/testcases/t_fstat.c
@@ -1,15 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <windows.h>
 
-int main(int argc, char *argv[])
+static char *path;
+
+static void remove_path(void) { RemoveDirectoryA(path); }
+
+int main()
 {
+    char temp[MAX_PATH + 1];
     struct stat st;
     struct stat64 st64;
-    if (0 == fstat(0, &st))
-        printf("mode = %x\n", st.st_mode);
-    if (0 == fstat64(0, &st64))
-        printf("mode = %x\n", st64.st_mode);
+    HANDLE handle;
+    int dirfd;
+
+    assert(fstat(0, &st) == 0);
+    printf("fstat(0): mode = %08o\n", st.st_mode);
+    assert(!S_ISDIR(st.st_mode));
+
+    assert(fstat64(0, &st64) == 0);
+    printf("fstat64(0): mode = %08o\n", st64.st_mode);
+    assert(!S_ISDIR(st64.st_mode));
+
+    assert(GetTempPathA(MAX_PATH, temp));
+    path = tempnam(temp, "mingw-w64-fstat-test-");
+    assert(path);
+    printf("CreateDirectoryA: path=%s\n", path);
+    assert(CreateDirectoryA(path, NULL));
+
+    printf("Open: path=%s\n", path);
+    handle = CreateFileA(path, FILE_READ_ATTRIBUTES | DELETE, FILE_SHARE_VALID_FLAGS, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_DELETE_ON_CLOSE, NULL);
+    if (handle != INVALID_HANDLE_VALUE)
+        atexit(remove_path);
+    assert(handle != INVALID_HANDLE_VALUE);
+
+    dirfd = _open_osfhandle((intptr_t)handle, O_RDONLY);
+    assert(dirfd >= 0);
+
+    assert(fstat(dirfd, &st) == 0);
+    printf("fstat(dirfd): mode = %08o\n", st.st_mode);
+    assert(S_ISDIR(st.st_mode));
+
+    assert(fstat64(dirfd, &st64) == 0);
+    printf("fstat64(dirfd): mode = %08o\n", st64.st_mode);
+    assert(S_ISDIR(st64.st_mode));
+
     return 0;
 }
 
