6ac1c2ff226283d60f9c7aaa60ae52c181e68a3d
Pali Rohár <pali.rohar@gmail.com>
2025-12-30 19:29:17 +0100

    crt: Improve __mingw_filename_cp() to work on systems without AreFileApisANSI() function
    It the AreFileApisANSI() function is not available then fallback to the
    default value that ANSI (ACP) encoding for filenames is used.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- mingw-w64-crt/misc/__mingw_filename_cp.c
+++ mingw-w64-crt/misc/__mingw_filename_cp.c
@@ -10,9 +10,20 @@
 #include <windows.h>
 #include <locale.h>
 
+/* By default the ANSI (ACP) is used, fallack to default ANSI when function AreFileApisANSI() is not available */
+static BOOL WINAPI fallbackAreFileApisANSI(VOID) { return TRUE; }
+
 unsigned int __cdecl __mingw_filename_cp(void)
 {
-  return (___lc_codepage_func() == CP_UTF8)
-         ? CP_UTF8
-         : AreFileApisANSI() ? CP_ACP : CP_OEMCP;
+  if (___lc_codepage_func() == CP_UTF8)
+    return CP_UTF8;
+
+  /* Function AreFileApisANSI() is not available in older Windows versions, so resolve it at runtime */
+  static BOOL (WINAPI *myAreFileApisANSI)(VOID) = NULL;
+  if (!myAreFileApisANSI) {
+    HMODULE kernel32 = GetModuleHandleA("kernel32.dll");
+    FARPROC farproc = kernel32 ? GetProcAddress(kernel32, "AreFileApisANSI") : NULL;
+    (void)InterlockedExchangePointer((PVOID*)&myAreFileApisANSI, farproc ?: fallbackAreFileApisANSI);
+  }
+  return myAreFileApisANSI() ? CP_ACP : CP_OEMCP;
 }
